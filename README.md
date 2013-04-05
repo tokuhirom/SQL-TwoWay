d6a2c157930190a547de54f04873f84b60271f4a
tokuhirom <tokuhirom@gmail.com>
2013-04-05 11:35:52 +0900

    Initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+SQL::TwoWay - It's new $module
+
+# SYNOPSIS
+
+    use SQL::TwoWay;
+
+# DESCRIPTION
+
+SQL::TwoWay is ...
+
+# LICENSE
+
+Copyright (C) tokuhirom
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+tokuhirom <tokuhirom@gmail.com>
---
+++ lib/SQL/TwoWay.pm
@@ -0,0 +1,34 @@
+package SQL::TwoWay;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=head1 NAME
+
+SQL::TwoWay - It's new $module
+
+=head1 SYNOPSIS
+
+    use SQL::TwoWay;
+
+=head1 DESCRIPTION
+
+SQL::TwoWay is ...
+
+=head1 LICENSE
+
+Copyright (C) tokuhirom
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tokuhirom E<lt>tokuhirom@gmail.comE<gt>
+
